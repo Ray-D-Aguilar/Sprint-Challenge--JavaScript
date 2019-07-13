9be92df04becaea3c40114158c1d01df8376ace5
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-13 12:10:54 -0500

    Created two functions, add and multiply. in their designated functions, two numbers will either be added or multiplied, and then the results will be passed to the callback

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,13 +7,33 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (param1, param2, cb) {
+  return cb(param1, param2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+//Function 1
+function add(num1, num2) {
+  //function add, will add two numbers (num1, num2) and passes the result to the callback
+  return num1 + num2;
+}
 
+let product = consume(9, 3, add);
+console.log(product);
+
+//Function 2
+function multiply(num1, num2) {
+  //function multiply will multiply two numbers(num1, num2) and passes the result to the callback
+  return num1 * num2;
+}
+
+let product2 = consume(9, 3, multiply);
+console.log(product2)
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
