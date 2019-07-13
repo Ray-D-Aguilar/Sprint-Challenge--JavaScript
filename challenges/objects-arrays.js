a56ba9e6b38802bd1127c39fef1ca52ed4d8b6d0
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-07-13 11:41:08 -0500

    Created an array for universities, and sorted othe list alphabetically

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,7 +12,10 @@ const tyrannosaurus = {
   diet: "carnivorous",
   weight: "7000kg",
   length: "12m",
-  period: "Late Cretaceous"
+  period: "Late Cretaceous",
+  roar() {
+    return  "RAWERSRARARWERSARARARRRR!";
+  }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -31,25 +34,26 @@ const velociraptor = {
   diet: "carnivorous",
   weight: "15kg",
   length: "1.8m",
-  period: "Late Cretaceous"
+  period: "Late Cretaceous",
+  
 }
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -71,6 +75,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for(let i = 0; i < graduates.length; i++ ) {
+  universities.push(graduates[i].university)
+}
+
+universities.sort();
+
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -80,6 +90,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+
+
+
 console.log(contactInfo);
 
 
